e6e18faca6bc037ea1b929dffe82b3721f7ea0b2
Muhamad Nauval Azhar <nauvalazhar2@gmail.com>
2020-04-17 19:49:45 +0700

    update config

---
+++ pages/config.js
@@ -0,0 +1,13 @@
+export default () => {
+	return (
+		<div>
+			<p>Muhamad Nauval Azhar is inviting you to a scheduled Zoom meeting.</p>
+
+			<p>Topic: Figma Fundamental</p>
+			<p>Time: Apr 18, 2020 10:00 AM Jakarta</p>
+
+			<p>Join Zoom Meeting</p>
+			<a href="https://zoom.us/j/93508018283">https://zoom.us/j/93508018283</a>
+		</div>
+	);
+}
